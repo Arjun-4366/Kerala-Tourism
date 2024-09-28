4aa3c8c829d99714c499cbc62e74758b341120b4
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-09-28 15:11:19 +0530

    New routes(wildLife,HillStations)

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import ThingTodo from "./pages/ThingTodo";
 import LoginPage from "./pages/LoginPage"
 import PlanMyTrip from "./pages/PlanMyTrip";
 import { useEffect, useState } from "react";
+import HillStations from "./components/ExperienceKerala/HillStations";
+import WildLife from "./components/ExperienceKerala/WildLife";
 
 
 function App() {
@@ -30,6 +32,9 @@ function App() {
 
         {/* <Route path="login" element = {<LoginPage/>}/> */}
         <Route path="explore" element = {<Experience loading={loading}/>}/>
+        <Route path="hillStations" element = {<HillStations loading={loading}/>}/>
+        <Route path="wildlife" element = {<WildLife loading={loading}/>}/>
+        
         <Route path="stay" element = {<WhereToStay loading={loading}/>}/>
         <Route path="thingsTodo" element = {<ThingTodo loading={loading}/>}/>
         <Route path="planMyTrip" element = {<PlanMyTrip loading={loading}/>}/>
