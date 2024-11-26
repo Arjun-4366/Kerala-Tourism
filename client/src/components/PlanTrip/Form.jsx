75401b142a54c9a83ad07d41739eb918c860e81d
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-11-26 07:11:11 +0530

    style adjustment

--- client/src/components/PlanTrip/Form.jsx
+++ client/src/components/PlanTrip/Form.jsx
@@ -117,7 +117,7 @@ function Form() {
         <div className="text-center px-3">
           <button
             type="submit"
-            className="px-4 py-2 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            className="px-4 py-2 bg-green-600 text-white rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
             Submit
           </button>
         </div>
