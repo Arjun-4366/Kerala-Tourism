1f7baae548b30d16bf9b6770eb43f50d8d7b4798
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-12-09 04:34:40 +0530

    added recomendation data

---
+++ client/src/data/recomendations.js
@@ -0,0 +1,19 @@
+const recommended = [
+    {
+        name:"Kovalam",
+        img:'./assets/recommend/kovalam.jpg',
+        rate:'₹2500'
+    },
+    {
+        name:"Idukki",
+        img:'./assets/recommend/idukki.avif',
+        rate:'₹3600'
+    },
+    {
+        name:"Kollam",
+        img:'./assets/recommend/kollam.webp',
+        rate:'₹3000'
+    },
+]
+
+export default recommended
