18d90ef6876afbb144e9859313673f7db4ebc21d
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-12-09 04:33:57 +0530

    added destination datas

---
+++ client/src/data/destination.js
@@ -0,0 +1,28 @@
+const destinations = [
+    {
+      name: "Munnar",
+      description:
+        "Munnar, the famed hill station of south India, is a romantic locale where natural beauty is everywhere to visit, explore, and enjoy. Munnar is situated at the confluence of three mountain streams – Muthirapuzha, Nallathanni, and Kundala – and the word ‘Munnar’ means three rivers in Malayalam. Situated about 1600m above sea level.",
+      video: "/assets/munnar.mp4",
+    },
+    {
+      name: "Varkala",
+      description:
+        "Varkala, a calm and quiet hamlet, lies on the outskirts of Thiruvananthapuram district. It has several tourist attractions that include a beautiful beach, a 2000-year-old Vishnu Temple, and the Ashramam - Sivagiri Mutt a little distance from the beach. Varkala offers excellent accommodation facilities for tourists and is fast becoming a popular health resort with many Ayurvedic massage centers.",
+      video: "/assets/varkala.mp4",
+    },
+    {
+      name: "Wayanad",
+      description:
+        "Wayanad, the green paradise is nestled among the mountains of the Western Ghats, forming the border world of the greener part of Kerala. Clean and pristine, enchanting and hypnotizing, this land is filled with history and culture. Located at a distance of 76 km from the sea shores of Kozhikode, this verdant hill station is full of plantations, forests, and wildlife.",
+      video: "/assets/wayanad.mp4",
+    },
+    {
+      name: "Alappuzha",
+      description:
+        "Officially called Alappuzha, Alleppey is known for its network of canals and lagoons with beautiful backwaters and the houseboats offering overnight stays. Located in the southern part of Kerala, the coastline of Alleppey offers some of the best beaches with water sports during the dry season.",
+      video: "/assets/Alleppy.mp4",
+    },
+  ];
+
+  export default destinations
